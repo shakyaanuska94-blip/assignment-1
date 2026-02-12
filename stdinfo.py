d9323426568c988f95aed2ae9df8b2112454274c
Anuska Shakya <shakyaanuska94@gmail.com>
2026-02-12 20:08:50 +0545

    initial project setup

---
+++ stdinfo.py
@@ -0,0 +1,25 @@
+print("Welocme to Student Management System")
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "admin" and password == "admin123":
+    print("\nLogin successful")
+    print("Role: Admin")
+
+    print("1. Add Student")
+    print("2. Delete Student")
+    print("3. Update Mark")
+    print("4. View/search student")
+    print("5. Sorting & Topper list")
+    print("6. Export to CSV")
+    
+elif username == "user" and password == "user123":
+    print("\nLogin sucessful")
+    print("Role: user")
+    
+    print("1. View/Search Stusents")
+    print("2. Sorting & Topper List")
+
+else:
+    print("Invalid username or password")
